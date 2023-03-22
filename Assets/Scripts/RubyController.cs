f1abbeab3935cdb38cfd516b816b1a44e6f4ad67
DiogoMaltaa <126678021+DiogoMaltaa@users.noreply.github.com>
2023-03-22 11:44:58 +0000

    World Interactions - Collectibles

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
 
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
     Rigidbody2D rb;
     float horizontal;
     float vertical;
@@ -13,6 +20,10 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -25,9 +36,15 @@ public class RubyController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 position = rb.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rb.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
